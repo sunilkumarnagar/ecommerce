5aebeae99f0b678fd4593ce515e9ec49f8f3cde2
sunilkumarnagar <sunilkumarnagar9929@gmail.com>
2026-06-11 23:17:09 +0530

    Add multiple pages using routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import HomePage from './pages/HomePage.jsx'
 import './App.css'
+import { Route, Routes} from 'react-router'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      <Route path="/" index element={<HomePage />} />
+      <Route path="checkout" element={<h1>Checkout Page</h1>} />
+    </Routes>
+  )
 }
 
 export default App
